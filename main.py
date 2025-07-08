7efe547f997776c8df4bdf53d04be157a1045a5a
kaustubh285 <kaustubh285@gmail.com>
2025-07-08 21:33:39 +0100

    Add BallTracker and CourtLineDetector classes for enhanced video analysis

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 from utils import log_function_call, read_video, save_video
-from trackers import PlayerTracker
+from trackers import PlayerTracker, BallTracker
+from court_line_detector import CourtLineDetector
 
 
 @log_function_call
 def main(input_path, output_path):
-    video_frames = read_video(input_path)
-    player_tracker = PlayerTracker(model_path="yolov8m")
 
+    video_frames = read_video(input_path)
+
+    # Initialize Trackers
+    player_tracker = PlayerTracker(model_path="yolov8m")
+    ball_tracker = BallTracker(model_path="models/best.pt")
+    court_line_detector = CourtLineDetector(model_path="models/keypoints_model.pth")
+
+    # Detections
     player_detections = player_tracker.detect_frames(
         video_frames,
-        read_from_stub=False,
+        read_from_stub=True,
         stub_path="tracker_stubs/player_detections.pkl",
     )
+    ball_detections = ball_tracker.detect_frames(
+        video_frames,
+        read_from_stub=True,
+        stub_path="tracker_stubs/ball_detections.pkl",
+    )
+    court_keypoints = court_line_detector.predict(video_frames[0])
 
+    # Draw
     updated_video = player_tracker.draw_bbox(video_frames, player_detections)
+    updated_video = ball_tracker.draw_bbox(updated_video, ball_detections)
+    updated_video = court_line_detector.draw_keypoints_video(
+        updated_video, court_keypoints
+    )
+
     save_video(updated_video, output_path)
 
 
---
+++ trackers/ball_tracker.py
@@ -0,0 +1,69 @@
+from ultralytics import YOLO
+from utils import log_function_call
+import cv2
+import pickle
+
+
+class BallTracker:
+    @log_function_call
+    def __init__(self, model_path: str):
+        self.model = YOLO(model_path)
+
+    @log_function_call
+    def detect_frames(
+        self,
+        frames,
+        stub_path,
+        read_from_stub: bool = False,
+    ) -> list:
+        ball_detections = []
+
+        if read_from_stub and stub_path is not None:
+            try:
+                with open(stub_path, "rb") as f:
+                    ball_detections = pickle.load(f)
+                return ball_detections
+            except FileNotFoundError:
+                print(f"Stub file {stub_path} not found. Processing frames...")
+
+        for frame in frames:
+            ball_dict = self.detect_frame(frame)
+            ball_detections.append(ball_dict)
+
+        if stub_path is not None:
+            with open(stub_path, "wb") as f:
+                pickle.dump(ball_detections, f)
+
+        return ball_detections
+
+    @log_function_call
+    def detect_frame(self, frame):
+        results = self.model.predict(frame, conf=0.15)[0]
+
+        ball_dict = {}
+        for box in results.boxes:
+            result = box.xyxy.tolist()[0]
+
+            ball_dict[1] = result
+
+        return ball_dict
+
+    def draw_bbox(self, frames, ball_detections) -> list:
+        """
+        Draw bounding boxes on the frames based on ball detections.
+        """
+        for frame, ball_dict in zip(frames, ball_detections):
+            for track_id, bbox in ball_dict.items():
+                x1, y1, x2, y2 = map(int, bbox)
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+                cv2.putText(
+                    frame,
+                    f"ball: {track_id}",
+                    (x1, y1 - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5,
+                    (0, 0, 255),
+                    2,
+                )
+
+        return frames
